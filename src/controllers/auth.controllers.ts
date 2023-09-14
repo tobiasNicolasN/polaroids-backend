202bfb99d8a19c20a36f9160f653a32da16e488e
Tobias Nuñez <tobias.nicolas001@gmail.com>
2023-09-14 11:41:54 -0300

    Register - insertId fixed

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,19 +1,28 @@
-import {Request, Response} from 'express'
-import bcryptjs from 'bcryptjs'
-import { dataBase } from '../db'
+import { Request, Response } from "express";
+import bcryptjs from "bcryptjs";
+import { dataBase } from "../db";
+import { OkPacket, OkPacketParams } from "mysql2";
 
-export const register = async (req: Request,res: Response) => {
-    const {username, email, password} = req.body
+export const register = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
 
-    try {
-        const passwordHash = await bcryptjs.hash(password, 10)
+  try {
+    const passwordHash = await bcryptjs.hash(password, 10);
 
-        const [rows] = await dataBase.query(`INSERT INTO users(username, email, password) VALUES (?, ?, ?)`, [username, email, passwordHash])
+    const [rows] = await dataBase.query(
+      `INSERT INTO users(username, email, password) VALUES (?, ?, ?)`,
+      [username, email, passwordHash]
+    );
+    const result = rows as OkPacket;
 
-        res.status(200).json({id: "fixThis", username: username, email: email, password: passwordHash})
-    }
-    catch (error) {
-        res.status(404)
-        console.log(error)
-    }
-}
+    res.status(200).json({
+      id: result.insertId,
+      username: username,
+      email: email,
+      password: passwordHash,
+    });
+  } catch (error) {
+    res.status(404);
+    console.log(error);
+  }
+};
