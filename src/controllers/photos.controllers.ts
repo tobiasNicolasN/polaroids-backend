c6259b8a2fc3dd365035989bd93f3fb26bcd3db0
Tobias Nuñez <tobias.nicolas001@gmail.com>
2023-09-18 12:52:08 -0300

    getPhotos controller finished

--- src/controllers/photos.controllers.ts
+++ src/controllers/photos.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { dataBase } from "../db";
 import { QueryError, ResultSetHeader } from "mysql2";
+import IPhotos from "../models/photos.models";
 
 export const postPhoto = async (req: Request, res: Response) => {
   const { title, photo } = req.body;
@@ -26,8 +27,15 @@ export const getPhoto = (_req: Request, res: Response) => {
   res.json({ m: "2" });
 };
 
-export const getPhotos = (_req: Request, res: Response) => {
-  res.json({ m: "3" });
+export const getPhotos = async (req: Request, res: Response) => {
+  try {
+  const [rows] = await dataBase.query<IPhotos[]>("SELECT * FROM photos WHERE user_id = ?", [req.user?.id] )
+  
+  res.status(200).json({photos: rows})
+  } catch (error) {
+    const err = error as QueryError
+    res.status(400).json({m: err.code})
+  }
 };
 
 export const deletePhoto = (_req: Request, res: Response) => {
