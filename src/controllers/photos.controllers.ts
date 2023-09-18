f984067c65a90ea30f24eb8cdc4101df09a4dac5
Tobias Nuñez <tobias.nicolas001@gmail.com>
2023-09-18 14:00:50 -0300

    Photos controllers finished
    - getPhoto controller

--- src/controllers/photos.controllers.ts
+++ src/controllers/photos.controllers.ts
@@ -23,8 +23,24 @@ export const postPhoto = async (req: Request, res: Response) => {
   }
 };
 
-export const getPhoto = (_req: Request, res: Response) => {
-  res.json({ m: "2" });
+export const getPhoto = async (req: Request, res: Response) => {
+  try {
+    const [rows] = await dataBase.query<IPhotos[]>(
+      "SELECT * FROM photos WHERE id = ?",
+      [req.params.id]
+    );
+    const result = rows[0];
+    res.status(200).json({
+      id: result.id,
+      title: result.title,
+      photo: result.photo,
+      created_at: result.created_at,
+      updated_at: result.updated_at,
+      user_id: result.user_id,
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Photo Not Found" });
+  }
 };
 
 export const getPhotos = async (req: Request, res: Response) => {
@@ -37,33 +53,44 @@ export const getPhotos = async (req: Request, res: Response) => {
     res.status(200).json({ photos: rows });
   } catch (error) {
     const err = error as QueryError;
-    res.status(400).json({ m: err.code });
+    res.status(400).json({ error: err.code });
   }
 };
 
 export const deletePhoto = async (req: Request, res: Response) => {
-  const [result] = await dataBase.query<ResultSetHeader>(
-    "DELETE FROM photos WHERE id = ?",
-    [req.params.id]
-  );
+  try {
+    const [result] = await dataBase.query<ResultSetHeader>(
+      "DELETE FROM photos WHERE id = ?",
+      [req.params.id]
+    );
 
-  if (result.affectedRows !== 0) {
-    res.sendStatus(204);
-  } else {
-    res.status(400).json({ message: "Photo Not found" });
+    if (result.affectedRows !== 0) {
+      res.sendStatus(204);
+    } else {
+      res.status(400).json({ message: "Photo Not found" });
+    }
+  } catch (error) {
+    const err = error as QueryError;
+    res.status(400).json({ error: err.code });
   }
 };
 
 export const updatePhoto = async (req: Request, res: Response) => {
-  const { title, photo } = req.body;
+  const { title } = req.body;
 
-  const [result] = await dataBase.query<ResultSetHeader>(
-    "UPDATE photos SET title = IFNULL(?, title), photo = IFNULL(?, photo) WHERE id = ?",
-    [title, photo, req.params.id]
-  );
-  if (result.affectedRows !== 0) {
-    res.json({ message: result.info });
-  } else {
-    res.status(400).json({ message: "Photo Not found" });
+  try {
+    const [result] = await dataBase.query<ResultSetHeader>(
+      "UPDATE photos SET title = ? WHERE id = ?",
+      [title, req.params.id]
+    );
+
+    if (result.affectedRows !== 0) {
+      res.sendStatus(204);
+    } else {
+      res.status(400).json({ message: "Photo Not found" });
+    }
+  } catch (error) {
+    const err = error as QueryError;
+    res.status(400).json({ error: err.code });
   }
 };
